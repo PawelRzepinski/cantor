3f746d42c5888986c90d6604a57d5b4486456cf8
unknown <pawel.rzepinski@gmail.com>
2021-02-01 17:38:56 +0100

    authenticate action, login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,32 @@
+import axios from 'axios';
+
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const UPDATE_CURRENCY = 'UPDATE_Currency';
+
+
+
+
 export const currencyUpdate = (currency) => {
     return {
-        type: 'UPDATE_CURRENCY',
+        type: UPDATE_CURRENCY,
         payload: {currency}
     }
-}
+};
+
+
+export const authenticate = (username, password) => dispatch => {
+    dispatch({ type: AUTH_REQUEST });
+
+    return axios
+        .post('http://localhost:9000/api/user/login', {
+            username,
+            password
+        })
+        .then(payload => {
+            console.log(payload);
+            dispatch({ type: AUTH_SUCCESS, payload });
+        })
+        .catch(error => console.log(error))
+};
+
